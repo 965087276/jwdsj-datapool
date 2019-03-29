5ed51f1587a080ea2cf91d0979ab2865f5bb2917
wangjinhao <965087276@qq.com>
2019-03-29 20:43:05 +0800

    初始化datapool-dictionary

--- datapool-dictionary/src/main/java/cn/ict/jwdsj/datapool/dictionary/repository/DictDatabaseRepo.java
+++ datapool-dictionary/src/main/java/cn/ict/jwdsj/datapool/dictionary/repository/DictDatabaseRepo.java
@@ -2,6 +2,8 @@ package cn.ict.jwdsj.datapool.dictionary.repository;
 
 import cn.ict.jwdsj.datapool.dictionary.entity.DictDatabase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DictDatabaseRepo extends JpaRepository<DictDatabase, Long> {
 }
--- datapool-dictionary/src/main/java/cn/ict/jwdsj/datapool/dictionary/repository/DictTableRepo.java
+++ datapool-dictionary/src/main/java/cn/ict/jwdsj/datapool/dictionary/repository/DictTableRepo.java
@@ -2,6 +2,8 @@ package cn.ict.jwdsj.datapool.dictionary.repository;
 
 import cn.ict.jwdsj.datapool.dictionary.entity.DictTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DictTableRepo extends JpaRepository<DictTable, Long> {
 }
