ec47fd3e17f3c9ce30328242b19ced7f3a6be964
wangjinhao <965087276@qq.com>
2019-05-01 11:00:41 +0800

    fix 空指针异常

--- datapool-datastats/src/main/java/cn/ict/jwdsj/datapool/datastat/web/StatsTableController.java
+++ datapool-datastats/src/main/java/cn/ict/jwdsj/datapool/datastat/web/StatsTableController.java
@@ -1,19 +1,27 @@
 package cn.ict.jwdsj.datapool.datastat.web;
 
+import cn.ict.jwdsj.datapool.common.entity.datastats.StatsTable;
+import cn.ict.jwdsj.datapool.datastat.service.StatsService;
 import cn.ict.jwdsj.datapool.datastat.service.StatsTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class StatsTableController {
 
     @Autowired
     private StatsTableService statsTableService;
+    @Autowired
+    private StatsService statsService;
 
     @GetMapping("stats/stats_table/records/tableId/{tableId}")
     long getTableRecords(@PathVariable("tableId") long tableId) {
-        return statsTableService.findByDictTableId(tableId).getTotalRecords();
+        return Optional.ofNullable(statsTableService.findByDictTableId(tableId))
+                .map(StatsTable::getTotalRecords)
+                .orElse(statsService.countTableRecords(tableId));
     }
 }
