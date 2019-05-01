77f9d405a7202acc6278bd268f02f6a247274c0b
wangjinhao <965087276@qq.com>
2019-05-01 10:39:21 +0800

    使用show columns命令代替COLUMNS表

--- datapool-common/src/main/java/cn/ict/jwdsj/datapool/common/entity/dictionary/meta/MetaColumn.java
+++ datapool-common/src/main/java/cn/ict/jwdsj/datapool/common/entity/dictionary/meta/MetaColumn.java
@@ -1,23 +1,17 @@
 package cn.ict.jwdsj.datapool.common.entity.dictionary.meta;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.*;
 
-@Entity
 @Data
-@Table(name = "COLUMNS", catalog = "INFORMATION_SCHEMA")
-@IdClass(MetaColumnMultiKeys.class)
+@AllArgsConstructor
 public class MetaColumn {
-    @Id
-    @Column(name = "TABLE_SCHEMA")
+
     private String database;
 
-    @Id
-    @Column(name = "TABLE_NAME")
     private String table;
 
-    @Id
-    @Column(name = "COLUMN_NAME")
     private String column;
 }
--- datapool-dictionary/src/main/java/cn/ict/jwdsj/datapool/dictionary/meta/repo/MetaColumnRepo.java
+++ datapool-dictionary/src/main/java/cn/ict/jwdsj/datapool/dictionary/meta/repo/MetaColumnRepo.java
@@ -1,14 +1,10 @@
 package cn.ict.jwdsj.datapool.dictionary.meta.repo;
 
 import cn.ict.jwdsj.datapool.common.entity.dictionary.meta.MetaColumn;
-import cn.ict.jwdsj.datapool.common.entity.dictionary.meta.MetaColumnMultiKeys;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface MetaColumnRepo extends JpaRepository<MetaColumn, MetaColumnMultiKeys> {
+public interface MetaColumnRepo {
     List<MetaColumn> findByDatabaseAndTable(String database, String table);
     List<MetaColumn> findByDatabaseAndTableIn(String database, List<String> tables);
 }
