9170efcf2f06d0a0425414816af1f6920e5e8458
wangjinhao <965087276@qq.com>
2019-05-03 19:15:05 +0800

    新建数据全览模块

---
+++ datapool-data_overview/src/main/java/cn/ict/jwdsj/datapool/overview/DataOverviewApplication.java
@@ -0,0 +1,11 @@
+package cn.ict.jwdsj.datapool.overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOverviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataOverviewApplication.class, args);
+    }
+}
