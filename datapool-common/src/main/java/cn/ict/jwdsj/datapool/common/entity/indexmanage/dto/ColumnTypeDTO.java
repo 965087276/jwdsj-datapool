0d1a0e4674929edd57bc5c53373c3a8b23e10497
wangjinhao <965087276@qq.com>
2019-08-16 15:31:42 +0800

    修改字段名称

--- datapool-common/src/main/java/cn/ict/jwdsj/datapool/common/entity/indexmanage/dto/ColumnTypeDTO.java
+++ datapool-common/src/main/java/cn/ict/jwdsj/datapool/common/entity/indexmanage/dto/ColumnTypeDTO.java
@@ -1,15 +1,19 @@
 package cn.ict.jwdsj.datapool.common.entity.indexmanage.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *  MappingColumn的封装，一个字段的esName及其type
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ColumnTypeDTO {
     private long columnId;
-    private String esColumn;
+    private String name;
     private String type;
 }
