a5269f19614db2cf531577de9bf2558221c18bf5
wangjinhao <965087276@qq.com>
2020-05-26 13:18:52 +0800

    修正feign依赖

--- datapool-api/src/main/java/cn/ict/jwdsj/datapool/api/ApiCommonConfig.java
+++ datapool-api/src/main/java/cn/ict/jwdsj/datapool/api/ApiCommonConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @Configuration
+@EnableFeignClients
 @ComponentScan(basePackageClasses = ApiCommonConfig.class)
 public class ApiCommonConfig {
 }
